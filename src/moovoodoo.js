2abbd57bdd6c0b678f4069fd6dd0fa82f08c885b
Pierre Renaudin <pierre.renaudin.fr@gmail.com>
2011-12-28 14:54:38 +0100

    add router function for moovoodoo

--- src/moovoodoo.js
+++ src/moovoodoo.js
@@ -1,9 +1,12 @@
- var Moovoodoo = {};
+
+(function() {
+
+var Moovoodoo = {};
 
 // -- Helpers
 
 // To use Static methods
-Class.Mutators.Static        = function(members){ this.extend(members);};
+Class.Mutators.Static = function(members){ this.extend(members);};
  
 var getUrl = function(object) {
   if (!(object && object.url)) return null;
@@ -44,20 +47,27 @@ Moovoodoo.Events = new Class({
 // -- Moovoodoo.Sync
 Moovoodoo.Sync = new Class({
   Static: {
+
+    // method called before sendind datas
+    prepareBeforeSync: function(options){
+      return options;
+    },
     
     create: function(model, options){
       !options && ( options = {} );
-      options.url = Moovoodoo.rootUrl + getUrl(model);
+      options.url    = Moovoodoo.rootUrl + getUrl(model);
       options.method = 'POST';
-      options.data   = model.toJSON();
+      options.data   = model.serialize();
+      options        = Moovoodoo.Sync.prepareBeforeSync(options);
       new Request.JSON(options).send();
     },
 
     read: function(model, params, options){
       !options && ( options = {} );
-      options.url = Moovoodoo.rootUrl + getUrl(model);
+      options.url    = Moovoodoo.rootUrl + getUrl(model);
       options.method = 'GET';
       options.data   = params;
+      options        = Moovoodoo.Sync.prepareBeforeSync(options);
       new Request.JSON(options).send();
     },
 
@@ -66,7 +76,8 @@ Moovoodoo.Sync = new Class({
       options.emulation = false;
       options.method    = 'PUT';
       options.url       = Moovoodoo.rootUrl + getUrl(model);
-      options.data      = model.toJSON();
+      options.data      = model.serialize();
+      options           = Moovoodoo.Sync.prepareBeforeSync(options);
       new Request.JSON(options).send(); 
     },
 
@@ -75,6 +86,7 @@ Moovoodoo.Sync = new Class({
       options.emulation = false;
       options.method    = 'DELETE';
       options.url       = Moovoodoo.rootUrl + getUrl(model);
+      options           = Moovoodoo.Sync.prepareBeforeSync(options);
       new Request.JSON(options).send();
     }
   }
@@ -218,15 +230,15 @@ Moovoodoo.Model = new Class({
     options || (options = {});
     var model = this;
     var onSuccess = options.onSuccess;
-    options.onSuccess = function(resp, status, xhr) {
-      if (!model.set(model.parse(resp), options)) return false;
+    options.onSuccess = function(resp) {
+      if (!model.set(resp, options)) return false;
       if (onSuccess) onSuccess(model, resp);
     };
     options.onError = wrapError(options.onError, model, options);
     if(this.sync)
       return this.sync.call(this, 'read', this, options);
     else
-      return Moovoodoo.Sync['read'](this, options);
+      return Moovoodoo.Sync['read'](this, {}, options);
   },
 
   changedAttributes : function(now) {
@@ -294,6 +306,11 @@ Moovoodoo.Model = new Class({
     return responseJSON;
   },
 
+  // serialize json before sending to the server
+  serialize: function(){
+    return this.toJSON();
+  },
+
   destroy : function(options) {
     options || (options = {});
     if (this.isNew()) return this.fireEvent('destroy', { model: this, collection: this.collection, options: options });
@@ -315,7 +332,7 @@ Moovoodoo.Model = new Class({
   },
 
   url : function() {
-    var base = getUrl(this.collection) || this.urlRoot || urlError();
+    var base = this.urlRoot || getUrl(this.collection) || urlError();
     if (this.isNew()) return base;
     return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + encodeURIComponent(this.id);
   },
@@ -373,14 +390,14 @@ Moovoodoo.Collection = new Class({
     return model;
   },
 
-  fetch: function(options){
+  fetch: function(params, options){
     options || (options = {});
     var onSuccess = options.onSuccess;
     options.onSuccess = function(resp){
       this[options.add ? 'add' : 'reset'](this.parse(resp),options);
       if(onSuccess) onSuccess(this, resp);
     }.bind(this);
-    return (this.sync || Moovoodoo.Sync).read(this,{},options);
+    return (this.sync || Moovoodoo.Sync).read(this, params, options);
   },
 
   reset : function(models, options) {
@@ -432,7 +449,7 @@ Moovoodoo.Collection = new Class({
   sort : function(options) {
     options || (options = {});
     if (!this.comparator) throw new Error('Cannot sort a set without a comparator');
-    this.models = this.sortBy(this.comparator);
+    this.models = this.sortBy(this.comparator.bind(this));
     if (!options.silent) this.fireEvent('reset', { collection: this, options: options} );
     return this;
   },
@@ -446,7 +463,7 @@ Moovoodoo.Collection = new Class({
     this._byId[model.id] = model;
     this._byCid[model.cid] = model;
     var index = options.at != null ? options.at :
-                this.comparator ? this.sortedIndex(model, this.comparator) :
+                this.comparator ? this.sortedIndex(model, this.comparator.bind(this)) :
                 this.length;
     this.models.splice(index, 0, model);
     model.addEvent('all', this._onModelEvent.bind(this));
@@ -506,7 +523,6 @@ Moovoodoo.Collection = new Class({
     }
     this.fireEvent(event.type,event);
   }
-
  
 });
 
@@ -528,31 +544,310 @@ Moovoodoo.Controller = new Class({
 
   eventSplitter : /^(\S+)\s*(.*)$/,
 
-  tagName : 'div',
+  tagName   : 'div',
+  className : '',
 
   initialize: function(){
     this.cid = _.uniqueId('view');
-    if(!this.element) this.element = new Element(this.tagName);
+    if(!this.element) this.element = new Element(this.tagName, { 'class': this.className });
+    this._delegatedEvents = [],
     this.delegateEvents();
+    this.refreshElements();
+  },
+
+  $: function(selector){
+    return (selector == null) ? this._getViewElement() : this._getViewElement().getElement(selector);
   },
 
   delegateEvents : function(events) {
     if (!(events || (events = this.events))) return;
     if (_.isFunction(events)) events = events.call(this);
+    this.undelegateEvents();
+    
     for (var key in events) {
       var method = this[events[key]];
       if (!method) throw new Error('Event "' + events[key] + '" does not exist');
       var match = key.match(this.eventSplitter);
       var eventName = match[1], selector = match[2];
       if (selector === '') {
-        this.element.addEvent(eventName, method.bind(this));
+        var name = eventName;
+        this._getViewElement().addEvent(name, method.bind(this));
       } else {
-        this.element.addEvent(eventName + ':relay(' + selector + ')', method.bind(this));
+        var name = eventName + ':relay(' + selector + ')';
+        this._getViewElement().addEvent(name, method.bind(this));
+      }
+      this._delegatedEvents.push({ name: name, method: method.bind(this) });
+    }
+  },
+
+  undelegateEvents: function(){
+    this._delegatedEvents.each(function(e){
+      this._getViewElement().removeEvent(e.name, e.method);
+    }.bind(this));
+    this._delegatedEvents = [];
+  },
+
+  refreshElements : function(elements){
+    if (!(elements || (elements = this.elements))) return;
+    for (var key in elements) {
+      var el = this._getViewElement().getElement(elements[key]);
+      this[key] = el;
+    }
+  },
+
+  _getViewElement: function(){
+    var el     = this.element;
+    if(!el) el = this.el;
+    if(!el) el = this.container;
+    return el;
+  }
+  
+});
+
+//-- Moovoodoo.Router
+Moovoodoo.Router = new Class({
+
+  Implements: Events,
+
+  // Cached regular expressions for matching named param parts and splatted
+  // parts of route strings.
+  namedParam    : /:([\w\d]+)/g,
+  splatParam    : /\*([\w\d]+)/g,
+  escapeRegExp  : /[-[\]{}()+?.,\\^$|#\s]/g,
+  
+  // Initialize is an empty function by default. Override it with your own
+  // initialization logic.
+  initialize : function(options){
+    options || (options = {});
+    if (options.routes) this.routes = options.routes;
+    this._bindRoutes();
+  },
+
+  // Manually bind a single named route to a callback. For example:
+  //
+  //     this.route('search/:query/p:num', 'search', function(query, num) {
+  //       ...
+  //     });
+  //
+  route : function(route, name, callback) {
+    Moovoodoo.history || (Moovoodoo.history = new Moovoodoo.History());
+    if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+    Moovoodoo.history.route(route, _.bind(function(fragment) {
+      var args = this._extractParameters(route, fragment);
+      callback && callback.apply(this, args);
+      this.fireEvent.apply(this, ['route:' + name].concat(args));
+    }, this));
+  },
+
+  // Simple proxy to `Backbone.history` to save a fragment into the history.
+  navigate : function(fragment, options) {
+    Moovoodoo.history.navigate(fragment, options);
+  },
+
+  // Bind all defined routes to `Moovoodoo.history`. We have to reverse the
+  // order of the routes here to support behavior where the most general
+  // routes can be defined at the bottom of the route map.
+  _bindRoutes : function() {
+    if (!this.routes) return;
+    var routes = [];
+    for (var route in this.routes) {
+      routes.unshift([route, this.routes[route]]);
+    }
+    for (var i = 0, l = routes.length; i < l; i++) {
+      this.route(routes[i][0], routes[i][1], this[routes[i][1]]);
+    }
+  },
+
+  // Convert a route string into a regular expression, suitable for matching
+  // against the current location hash.
+  _routeToRegExp : function(route) {
+    route = route.replace(this.escapeRegExp, "\\$&")
+                 .replace(this.namedParam, "([^\/]*)")
+                 .replace(this.splatParam, "(.*?)");
+    return new RegExp('^' + route + '$');
+  },
+
+  // Given a route, and a URL fragment that it matches, return the array of
+  // extracted parameters.
+  _extractParameters : function(route, fragment) {
+    return route.exec(fragment).slice(1);
+  }
+
+});
+
+//-- fix for mootools : add event 'hashchange'
+Element.Events.hashchange = {
+    onAdd: function () {
+      var hash = location.hash;
+      var hashchange = function () {
+        if (hash == location.hash) return;
+        else hash = location.hash;
+
+        var value = (hash.indexOf('#') == 0 ? hash.substr(1) : hash);
+        window.fireEvent('hashchange', value);
+        document.fireEvent('hashchange', value);
+      };
+
+      if ("onhashchange" in window) {
+          window.onhashchange = hashchange;
       }
     }
+};
+
+
+//-- Moovoodoo.History
+Moovoodoo.History = new Class({
+
+  // Cached regex for cleaning hashes.
+  hashStrip: /^#/,
+
+  // Has the history handling already been started?
+  historyStarted: false,
+
+  // The default interval to poll for hash changes, if necessary, is
+  // twenty times a second.
+  interval: 50,
+
+  initialize: function(){
+    this.handlers = [];
+  },
+
+  // Get the cross-browser normalized URL fragment, either from the URL,
+  // the hash, or the override.
+  getFragment : function(fragment, forcePushState) {
+    if (fragment == null) {
+      if (this._hasPushState || forcePushState) {
+        fragment = window.location.pathname;
+        var search = window.location.search;
+        if (search) fragment += search;
+      } else {
+        fragment = window.location.hash;
+      }
+    }
+    fragment = decodeURIComponent(fragment.replace(this.hashStrip, ''));
+    if (!fragment.indexOf(this.options.root)) fragment = fragment.substr(this.options.root.length);
+    return fragment;
+  },
+
+  // Start the hash change handling, returning `true` if the current URL matches
+  // an existing route, and `false` otherwise.
+  start : function(options) {
+
+    // Figure out the initial configuration. Do we need an iframe?
+    // Is pushState desired ... is it available?
+    if (this.historyStarted) throw new Error("Moovoodoo.history has already been started");
+    this.options          = _.extend({}, {root: '/'}, this.options, options);
+    this._wantsPushState  = !!this.options.pushState;
+    this._hasPushState    = !!(this.options.pushState && window.history && window.history.pushState);
+    var fragment          = this.getFragment();
+    var docMode           = document.documentMode;
+    var oldIE             = Browser.ie && Browser.version <= 7;
+    if (oldIE) {
+      this.iframe = new Element('iframe', { src: 'javascript:0', tabindex: '-1' } ).hide().inject(document.body).contentWindow;
+      this.navigate(fragment);
+    }
+
+    // Depending on whether we're using pushState or hashes, and whether
+    // 'onhashchange' is supported, determine how we check the URL state.
+    if (this._hasPushState) {
+      window.addEvent('popstate', this.checkUrl.bind(this));
+    } else if ('onhashchange' in window && !oldIE) {
+      window.addEvent('hashchange', this.checkUrl.bind(this));
+    } else {
+      setInterval(this.checkUrl.bind(this), this.interval);
+    }
+
+    // Determine if we need to change the base url, for a pushState link
+    // opened by a non-pushState browser.
+    this.fragment = fragment;
+    historyStarted = true;
+    var loc = window.location;
+    var atRoot  = loc.pathname == this.options.root;
+    if (this._wantsPushState && !this._hasPushState && !atRoot) {
+      this.fragment = this.getFragment(null, true);
+      window.location.replace(this.options.root + '#' + this.fragment);
+      // Return immediately as browser will do redirect to new url
+      return true;
+    } else if (this._wantsPushState && this._hasPushState && atRoot && loc.hash) {
+      this.fragment = loc.hash.replace(this.hashStrip, '');
+      window.history.replaceState({}, document.title, loc.protocol + '//' + loc.host + this.options.root + this.fragment);
+    }
+
+    if (!this.options.silent) {
+      return this.loadUrl();
+    }
+  },
+
+  // Add a route to be tested when the fragment changes. Routes added later may
+  // override previous routes.
+  route : function(route, callback) {
+    this.handlers.unshift({route : route, callback : callback});
+  },
+
+  // Checks the current URL to see if it has changed, and if it has,
+  // calls `loadUrl`, normalizing across the hidden iframe.
+  checkUrl : function(e) {
+    var current = this.getFragment();
+    if (current == this.fragment && this.iframe) current = this.getFragment(this.iframe.location.hash);
+    if (current == this.fragment || current == decodeURIComponent(this.fragment)) return false;
+    if (this.iframe) this.navigate(current);
+    this.loadUrl() || this.loadUrl(window.location.hash);
+  },
+
+  // Attempt to load the current URL fragment. If a route succeeds with a
+  // match, returns `true`. If no defined routes matches the fragment,
+  // returns `false`.
+  loadUrl : function(fragmentOverride) {
+    var fragment = this.fragment = this.getFragment(fragmentOverride);
+    var matched = _.any(this.handlers, function(handler) {
+      if (handler.route.test(fragment)) {
+        handler.callback(fragment);
+        return true;
+      }
+    });
+    return matched;
+  },
+
+  // Save a fragment into the hash history, or replace the URL state if the
+  // 'replace' option is passed. You are responsible for properly URL-encoding
+  // the fragment in advance.
+  //
+  // The options object can contain `trigger: true` if you wish to have the
+  // route callback be fired (not usually desirable), or `replace: true`, if
+  // you which to modify the current URL without adding an entry to the history.
+  navigate : function(fragment, options) {
+    if (!options || options === true) options = {trigger: options};
+    var frag = (fragment || '').replace(this.hashStrip, '');
+    if (this.fragment == frag || this.fragment == decodeURIComponent(frag)) return;
+    if (this._hasPushState) {
+      if (frag.indexOf(this.options.root) != 0) frag = this.options.root + frag;
+      this.fragment = frag;
+      window.history[options.replace ? 'replaceState' : 'pushState']({}, document.title, frag);
+    } else {
+      this.fragment = frag;
+      this._updateHash(window.location, frag, options.replace);
+      if (this.iframe && (frag != this.getFragment(this.iframe.location.hash))) {
+        // Opening and closing the iframe tricks IE7 and earlier to push a history entry on hash-tag change.
+        // When replace is true, we don't want this.
+        if(!options.replace) this.iframe.document.open().close();
+        this._updateHash(this.iframe.location, frag, options.replace);
+      }
+    }
+    if (options.trigger) this.loadUrl(fragment);
+  },
+
+  // Update the hash location, either replacing the current entry, or adding
+  // a new one to the browser history.
+  _updateHash: function(location, fragment, replace) {
+    if (replace) {
+      location.replace(location.toString().replace(/(javascript:|#).*$/, "") + "#" + fragment);
+    } else {
+      location.hash = fragment;
+    }
   }
-	
+
 });
+
 
 // Wrap an optional error callback with a fallback error event.
 var wrapError = function(onError, originalModel, options) {
@@ -570,3 +865,6 @@ var urlError = function() {
   throw new Error('A "url" property or function must be specified');
 };
 
+window.Moovoodoo = Moovoodoo;
+
+}).call(this);
